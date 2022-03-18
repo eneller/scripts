7918a3b9aafdada6d27329a2b1eeec6d546d4c0e
eneller <erikneller@gmx.de>
2022-03-18 22:47:57 +0100

    (mymuesli)remove unnecessary println

--- mymuesli/main.java
+++ mymuesli/main.java
@@ -14,7 +14,6 @@ public class Main{
     public static int length = 6;
 
     public static void main(String[] args) throws IOException {
-    	System.out.println("test");
         String filepath = "https://www.mymuesli.com/";
 
 
