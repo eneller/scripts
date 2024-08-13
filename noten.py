cbbe50878c6588a71e578c57808ac03cd8876bb9
eneller <erikneller@gmx.de>
2024-08-14 01:02:41 +0200

    minor stuff

--- noten.py
+++ noten.py
@@ -13,7 +13,7 @@ data.sort_values(by='note', inplace=True)
 acc_note = 0.0
 acc_lp = 0
 # the use of iterrows and all iteration over dataframes is discouraged for performance reasons
-for index, row in data.iterrows():
+for _ , row in data.iterrows():
     if acc_lp + row['lp'] < target_lp:
         acc_lp = acc_lp + row['lp']
         acc_note = acc_note + row['lp'] * row['note']
